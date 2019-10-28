89973406b9a6adc29efcbb4e8a0b3e677b632bde
unknown <lhnguyen38@uh.edu>
2019-10-28 16:03:51 -0500

    got nested form working

--- app/models/physician.rb
+++ app/models/physician.rb
@@ -1,4 +1,4 @@
 class Physician < ApplicationRecord
     has_many :offices, inverse_of: :physician
-    accepts_nested_attributes_for :offices, reject_if: :all_blank, allow_destroy:true
+    accepts_nested_attributes_for :offices, reject_if: :all_blank, allow_destroy: true
 end

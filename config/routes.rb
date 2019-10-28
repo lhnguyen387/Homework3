6a943344a84e034853529f88e2cc68bcd4872339
unknown <lhnguyen38@uh.edu>
2019-10-28 15:13:48 -0500

    cocoon setup, misc edits

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :offices
+  resources :appointments
+  resources :physicians
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
